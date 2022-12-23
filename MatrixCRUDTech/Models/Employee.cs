e78dce80e5d2e9a335745df49db7b22fb5f7ea2a
Eduardo <edugramosf@gmail.com>
2022-12-23 11:17:51 -0300

    Add project files.

---
+++ MatrixCRUDTech/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatrixCRUDTech.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage="Este campo é de preenchimento obrigatório.")]
+        [DisplayName("Nome")]
+        public string EmployeeFullName { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        [DisplayName("Matrícula")]
+        public int EmpCode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Cargo")]
+
+        public int Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Localização")]
+        public int OfficeLocation { get; set; }
+        
+
+
+    }
+}
